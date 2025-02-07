824ba6af989606017c16f2d36c1d688247487c7d
Yisus <jesus.pena@ebconsulting.co>
2025-02-07 13:39:58 -0500

    add(fastapi): 04 Parámetros de ruta y consultas en FastAPI

---
+++ FastAPIProject/main.py
@@ -0,0 +1,27 @@
+import zoneinfo
+from datetime import datetime
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello Worlds"}
+
+country_timezones = {
+    "US": "America/New_York",
+    "UK": "Europe/London",
+    "DE": "Europe/Berlin",
+    "FR": "Europe/Paris",
+    "CO": "America/Bogota",
+    "MX": "America/Mexico_City",
+}
+
+@app.get("/time/{iso_code}")
+async def time(iso_code: str):
+    iso = iso_code.upper()
+    timezone_str = country_timezones.get(iso)
+    tz = zoneinfo.ZoneInfo(timezone_str)
+    return {"time": datetime.now(tz)}
