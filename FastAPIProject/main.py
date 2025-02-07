0778b72d2e1ad287ec48691c7b1a275cd28866e1
Yisus <jesus.pena@ebconsulting.co>
2025-02-07 15:58:42 -0500

    add(fastapi): 11 Crear un CRUD básico en FastAPI: Actualizar

--- FastAPIProject/main.py
+++ FastAPIProject/main.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from fastapi import FastAPI, HTTPException, status
 from sqlmodel import select
 
-from models import Customer, CustomerCreate, Transaction, Invoice
+from models import Customer, CustomerCreate, CustomerUpdate, Transaction, Invoice
 from db import SessionDep, create_all_tables
 
 app = FastAPI(lifespan=create_all_tables)
@@ -66,6 +66,20 @@ async def delete_customer(customer_id: int, session: SessionDep):
     session.delete(customer_db)
     session.commit()
     return {"detail": "Customer deleted"}
+
+
+@app.patch("/customers/{customer_id}", response_model=Customer, status_code=status.HTTP_201_CREATED)
+async def update_customer(customer_id: int, customer_data: CustomerUpdate, session: SessionDep):
+    customer_db = session.get(Customer, customer_id)
+    if not customer_db:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="Customer not found")
+    customer_data_dict = customer_data.model_dump(exclude_unset=True)
+    customer_db.sqlmodel_update(customer_data_dict)
+    session.add(customer_db)
+    session.commit()
+    session.refresh(customer_db)
+    return customer_db
 
 
 @app.post("/transactions")
